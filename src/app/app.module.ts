5a5f1bb57f9b4788db22fa5357d7021377c824c3
Samyr <saamyr@live.com>
2020-06-08 22:35:21 -0300

    Primeiro commit, projeto iniciado desde sabado 06/06

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { environment } from '../environments/environment';
+import { LoginComponent } from './components/public/login/login.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+    firebaseConfig: {
+    apiKey: "AIzaSyBixTmtNnYgexkCxhrsALQZypm3EaEFeA4",
+    authDomain: "requisicoes-app-c9100.firebaseapp.com",
+    databaseURL: "https://requisicoes-app-c9100.firebaseio.com",
+    projectId: "requisicoes-app-c9100",
+    storageBucket: "requisicoes-app-c9100.appspot.com",
+    messagingSenderId: "460339306940",
+    appId: "1:460339306940:web:fa2a39b33c719f38b34bb9",
+    measurementId: "G-2X69JELJZ4"
+    }
 };
 
 /*
