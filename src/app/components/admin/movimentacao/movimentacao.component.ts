012ef533d37ee74d45b3ddd2ce6f4dc31e3b676a
SamyrOR <saamyr@live.com>
2021-08-26 23:49:23 -0300

    Done movimentacoes methods

--- src/app/components/admin/movimentacao/movimentacao.component.ts
+++ src/app/components/admin/movimentacao/movimentacao.component.ts
@@ -5,6 +5,7 @@ import { map } from 'rxjs/operators';
 import { Funcionario } from 'src/app/models/funcionario.model';
 import { Movimentacao, Requisicao } from 'src/app/models/requisicao.model';
 import { RequisicaoService } from 'src/app/services/requisicao.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-movimentacao',
@@ -63,14 +64,74 @@ export class MovimentacaoComponent implements OnInit {
       );
   }
 
+  setValorPadrao() {
+    this.form.patchValue({
+      funcionario: this.funcionarioLogado,
+      dataHora: new Date(),
+    });
+    this.movimentacoes = [];
+  }
+
   add(requisicao: Requisicao) {
     this.form.reset();
     this.edit = false;
-    // this.setValorPadrao();
+    this.setValorPadrao();
     this.requisicaoSelecionada = requisicao;
     this.movimentacoes = !requisicao.movimentacoes
       ? []
       : requisicao.movimentacoes;
     this.displayDialogMovimentacao = true;
+  }
+
+  save() {
+    this.movimentacoes.push(this.form.value);
+    this.requisicaoSelecionada.movimentacoes = this.movimentacoes;
+    this.requisicaoSelecionada.status = this.form.controls['status'].value;
+    this.requisicaoSelecionada.ultimaAtualizacao = new Date();
+    this.requisicaoService
+      .createOrUpdate(this.requisicaoSelecionada)
+      ?.then(() => {
+        this.displayDialogMovimentacao = false;
+        Swal.fire(
+          `Requisição ${!this.edit ? 'salva' : 'atualizada'} com sucesso.`,
+          '',
+          'success'
+        );
+      })
+      .catch((erro) => {
+        this.displayDialogMovimentacao = true;
+        Swal.fire(
+          `Erro ao ${!this.edit ? 'salvar' : 'atualizar'} a Requisição.`,
+          `Detalhes: ${erro}`,
+          'error'
+        );
+      });
+    this.form.reset();
+  }
+
+  onDialogClose(event: any) {
+    this.displayDialogMovimentacoes = event;
+  }
+
+  verMovimentacoes(requisicao: Requisicao) {
+    this.requisicaoSelecionada = requisicao;
+    this.movimentacoes = requisicao.movimentacoes;
+    this.displayDialogMovimentacoes = true;
+  }
+  delete(requisicao: Requisicao) {
+    Swal.fire({
+      title: 'Confirma a exclusão da Requisição?',
+      text: '',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sim',
+      cancelButtonText: 'Não',
+    }).then((result) => {
+      if (result.value) {
+        this.requisicaoService.delete(requisicao.id).then(() => {
+          Swal.fire('RequisiÇão excluida com sucesso', '', 'success');
+        });
+      }
+    });
   }
 }
