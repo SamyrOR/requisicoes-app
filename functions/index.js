16761ad1fa093f94efeab65d441526d89b9fb601
SamyrOR <saamyr@live.com>
2021-08-30 23:12:39 -0300

    Project done with firebase functions and hosting

--- functions/index.js
+++ functions/index.js
@@ -12,3 +12,67 @@ const nodemailer = require("nodemailer");
 // });
 
 admin.initializeApp();
+
+let mailTransport = nodemailer.createTransport({
+  server: "gmail",
+  auth: {
+    user: "requicioesapp12@gmail.com",
+    pass: "Requisicoes12",
+  },
+});
+
+exports.createUser = functions.firestore
+  .document("/funcionarios/{documentId}")
+  .onCreate((snap, context) => {
+    const functionario = snap.data();
+    const email = functionario.email;
+    const nome = functionario.nome;
+    return admin
+      .auth()
+      .createUser({
+        uid: `${email}`,
+        email: `${email}`,
+        emailVerified: false,
+        password: `123456`,
+        displayName: `${nome}`,
+        disabled: false,
+      })
+      .then((userRecord) => {
+        console.log(`Usuário registrado com sucesso.`);
+        return userRecord;
+      })
+      .catch(function (error) {
+        console.log("Não foi possível criar o usuário", error);
+      });
+  });
+
+exports.notifyUser = functions.firestore
+  .document("/requisicoes/{documentId}")
+  .onUpdate((snap, context) => {
+    const requisicao = snap.after.data();
+    const solicitante = requisicao.solicitante;
+    const email = solicitante.email;
+    const movimentacoes = requisicao.movimentacoes;
+    if (movimentacoes.length > 0) {
+      const movimentacao = movimentacoes[movimentacoes.length - 1];
+      console.log(movimentacao);
+      const text = `<h2>Sua requisição recebeu uma atualização!</h2>
+      <h3>Descricao: ${movimentacao.descricao}</h3>
+      <h4>Status: ${movimentacao.status}</h4>`;
+      const mailOptions = {
+        from: `<noreply@firebase.com>`,
+        to: email,
+        subject: `Sistema de Requisições | Processamento de Requisições`,
+        html: `${text}`,
+      };
+      return mailTransport
+        .sendMail(mailOptions)
+        .then(() => {
+          console.log("Email enviado para: ", email);
+          return null;
+        })
+        .catch((error) => {
+          console.log("Não foi possivel notificar o usuário: ", error);
+        });
+    }
+  });
