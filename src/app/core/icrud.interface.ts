3dd27482f80151d525367f6d28cdb1afdb8edd81
SamyrOR <saamyr@live.com>
2021-09-01 12:54:55 -0300

    Fixed folders structure

--- src/app/core/icrud.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Observable } from 'rxjs';
-
-export interface ICrud<T> {
-  get(id: string): Observable<T>;
-  list(): Observable<T[]>;
-  createOrUpdate(item: T): Promise<any> | undefined;
-  delete(id: string): Promise<void>;
-}
